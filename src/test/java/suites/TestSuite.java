91cebbb8b5e6937c56c173de137b44104a8aafde
Anna <a.mironyuk@unlimint.com>
2022-12-10 18:16:11 +0200

    jenkins

---
+++ src/test/java/suites/TestSuite.java
@@ -0,0 +1,15 @@
+package suites;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import tests.ArticleTests;
+import tests.ChangeAppConditionTests;
+import tests.SearchTests;
+@RunWith(Suite.class)
+@Suite.SuiteClasses({
+        ArticleTests.class,
+        ChangeAppConditionTests.class,
+        SearchTests.class
+})
+public class TestSuite {
+}
